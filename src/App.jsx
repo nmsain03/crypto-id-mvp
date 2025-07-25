7d3845e6592a3507c415ec53f254404434a44f6e
nmsain03 <148716732+nmsain03@users.noreply.github.com>
2025-07-25 17:52:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function WalletCard({ chain, address }) {
         className="absolute inset-0 z-0 opacity-10 bg-repeat"
          style={{
           backgroundImage: 'url("/btc.png")',
-          backgroundSize: '60px 60px', // mehr Abstand
+          backgroundSize: '80px 80px', // mehr Abstand
           backgroundPosition: '0 0, 32px 32px', // (optional layering)
           animation: 'scroll-diagonal 30s linear infinite',
           filter: 'blur(3px)' // unschärfe für glossy Effekt
