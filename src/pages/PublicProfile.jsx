e9a9bdeb67f37cc2952bbcfa51f06dadf687dc15
nmsain03 <148716732+nmsain03@users.noreply.github.com>
2025-07-26 20:51:23 +0200

    Update PublicProfile.jsx

--- src/pages/PublicProfile.jsx
+++ src/pages/PublicProfile.jsx
@@ -1,1 +1,9 @@
+// src/pages/PublicProfile.jsx
+export default function PublicProfile() {
+  return (
+    <div>
+      {/* dein bisheriger JSX mit Profilbild, Wallet Cards etc. */}
+    </div>
+  );
+}
 
