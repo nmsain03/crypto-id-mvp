2af82ca4e243b159e985bf85b89e9ee018e11297
nmsain03 <148716732+nmsain03@users.noreply.github.com>
2025-07-27 22:25:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import PublicProfile from "./pages/PublicProfile.jsx";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<PublicProfile />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  );
-}
-
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-
-function App() {
-  return (
     <ThirdwebProvider activeChain="ethereum">
-      {/* dein Layout oder Routes */}
+      <Routes>
+        <Route path="/" element={<PublicProfile />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </ThirdwebProvider>
   );
 }
